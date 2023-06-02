1d9bbcf8054ebbdf736afc2cb68167a2c04eff12
david.germ <david.germ@kiwi.com>
2023-06-02 11:18:48 +0200

    main: add test for CZ language

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FAQbot
 Skript for testing FAQbot in Kiwi
+
+## HOW TO RUN:
+
+Program require one argument `lang` which define which language version you want to test (`cz` or `en`)
+
+```commandline
+python3 faqbot.py <lang>
+```
--- faqbot.py
+++ faqbot.py
@@ -4,51 +4,68 @@ import sys
 
 
 def main():
-    file_name = sys.argv[1]
+    lang = sys.argv[1]
+    file_name = "queries_en.txt"
+    file_name2 = "queries_cs.txt"
     url = "https://kiwi.aicheck.tech/"
+    url2 = "https://kiwi2.aicheck.tech/"
     headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic cGlsb3Q6UDFsMFROb3RTb1ZlcnlMb25nUGFzc3dvcmQ='
+        'Content-Type': 'application/json'
     }
-    queries = get_queries(file_name)
-    print(queries)
-    # queriesCount = len(queries)
-    # print(queriesCount)
-    #get_payload(url, headers, queries)
+    if lang == "en":
+        queries = get_queries(file_name)
+        answers = get_payload(url, headers, queries)
+    else:
+        queries = get_queries(file_name2)
+        answers = get_payload(url2, headers, queries)
+    save_to_txt(answers)
+    if answers:
+        create_csv(queries, answers)
 
 
 def get_payload(url, headers, queries):
     answers = []
+    username = "pilot"
+    password = "P1l0TNotSoVeryLongPassword"
+    auth = (username, password)
 
     for query in queries:
         payload = json.dumps({
             "query": query
         })
-        response = requests.request("POST", url, headers=headers, data=payload)
+        response = requests.request("POST", url, headers=headers, data=payload, auth=auth)
         if response.status_code == 200:
             results = json.loads(response.text)
             answers.append(results["answer"])
 
-    save_to_txt(answers)
-    # print(answers)
-    # print(type(answers))
+    return answers
 
 
 def get_queries(file_name):
     queries = []
     with open(file_name, 'r', encoding='UTF-8') as file:
-        while line := file.readline():
-            print(line.rstrip())
-            queries.append(line)
-
+        for line in file:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            else:
+                queries.append(line)
     return queries
 
 
 def save_to_txt(answers):
-    with open("queries.txt", "w", encoding="utf-8", newline="\n") as f:
+    with open("answers.txt", "w", encoding="utf-8", newline="\n") as f:
         for line in answers:
             f.write(line)
             f.write("\n")
+
+
+def create_csv(queries, answers):
+    with open('my_file.csv', 'w') as csv_file:
+        for i in range(min(len(queries), len(answers))):
+            query = queries[i].strip()
+            answer = answers[i].strip()
+            csv_file.write(f'{query},{answer}\n')
 
 
 if __name__ == "__main__":
